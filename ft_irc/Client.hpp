d9b88803a8597836a705ed5218aceb4ef2480380
andrijajovanovic98 <andrija.jovanovic1998@gmail.com>
2024-12-23 02:03:31 +0100

    CLIENT

--- ft_irc/Client.hpp
+++ ft_irc/Client.hpp
@@ -29,41 +29,39 @@ private:
 public:
 
 	int kick;
- 	Client(int fd, const std::string& nickname);
+	const std::vector<std::string>& getOnChannels() const;
+ 	
+	Client(int fd, const std::string& nickname);
 
     std::string getCurrentChannel() const;
 	std::string getNickname() const;
 	std::string getUserName() const;
 	std::string getRealName() const;
-	int getFd() const;
 	std::string getIpAddress() const;
+	std::string getBuffer() const;
+	int getFd() const;
     int getDccPort() const;
 
 	void setNickname(const std::string &nickname);
 	void setUsername(const std::string &userName);
+ 	void setCurrentChannel(const std::string &channel);
+	void setClientAuthenticated(bool value);
 	std::string generateUsername(int fd);
 
- 	void setCurrentChannel(const std::string &channel);
-
-	bool sendMessage(const std::string& message) const; 
-
 	bool isClientAuthenticated() const;
-	void setClientAuthenticated(bool value);
 
 	void AddChannelToList(const std::string& channelName);
-	void broadcastNickChange(const std::string& oldNick, const std::string& newNick, Server* server);
 
-	const std::vector<std::string>& getOnChannels() const;
 	void RemoveGetOnChannels(const std::string &channelName);
 	bool FindGetOnChannels(const std::string &channelName);
 
-	// Client buffer
-
-	void appendToBuffer(const std::string& data);
-	std::string getBuffer() const;
-	bool hasCompleteCommand() const;
 	std::string extractCommand();
 	void clearBuffer();
+	void appendToBuffer(const std::string& data);
+	bool hasCompleteCommand() const;
+
+	bool sendMessage(const std::string& message) const; 
+	void broadcastNickChange(const std::string& oldNick, const std::string& newNick, Server* server);
 };
 
 int stringToInt(const std::string& str);
